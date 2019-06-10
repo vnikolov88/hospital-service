18da026e2b367b83e06b972211cf66adaacd8190
Veselin Nikolov <vnikolov88@gmail.com>
2019-06-10 22:39:39 +0300

    Added models for social information

---
+++ hospital-service-contracts/V2/SocialInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalService.Contracts.V2
+{
+    public class SocialInformation
+    {
+        public string FBUrl { get; set; }
+        public string TWUrl { get; set; }
+        public string DataPrivacyUrl { get; set; }
+        public string ImprintUrl { get; set; }
+        public string JobsUrl { get; set; }
+        public string NewsUrl { get; set; }
+        public string EventsUrl { get; set; }
+    }
+}
